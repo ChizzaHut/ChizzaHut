d80862cedae73957a7f4f7a5faa2274b8d159461
ChizzaHut <87155392+ChizzaHut@users.noreply.github.com>
2021-08-11 09:33:59 -0400

    2nd update
    removed string format for variable base2

--- cashregister.py
+++ cashregister.py
@@ -1,7 +1,8 @@
 item1 = float(input("Enter price of the first item:"))
 item2 = float(input("Enter price of the second item:"))
 base = item1 + item2
-base = "{:.2f}".format(base)
+base = "{:,.2f}".format(base)
+
 
 a = min(item1, item2)
 b = max(item1, item2)
@@ -11,13 +12,20 @@ base2 = a1 + b
 
 tax = float(input("Enter tax rate:")) / 100
 
-member = input('Do you have a club card? (Y/N)')
+member = str(input('Do you have a club card?'))
 
-if member == "Y" or "y" or "yes":
-    discount = round(base2 - (base2*.1))
+total2 = base2 + (base2 * tax)
+total2 = "{:,.2f}".format(total2)
+
+if member == ('y' or 'Y' or 'yes'):
+    discount = (base2 - (base2 * 0.1))
+    total = float(discount + discount * tax)
+    total = "{:,.2f}".format(total)
+
     print("Base price =", base)
     print("Price after discounts =", discount)
-    print("Total price =", round(discount + discount * tax, 2))
-else:
+    print("Total price =", total)
+
+elif member != ('y' or 'Y' or 'yes'):
     print("Base price =", base)
-    print("Total price = ", round(base2 + base2 * tax, 2))
+    print("Total price = ", total2)
