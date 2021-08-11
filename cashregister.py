9c7b07811e0e66aff76c3f543c8c91cff7e10a93
ChizzaHut <87155392+ChizzaHut@users.noreply.github.com>
2021-08-11 13:47:21 -0400

    update 15
    added member variables and if statement

--- cashregister.py
+++ cashregister.py
@@ -8,24 +8,39 @@ b = max(item1, item2)
 a1 = a*.5
 
 base2 = a1 + b
+# Make variables for Y and N
+member = input('Does customer have a club card? (Y/N):')
+# Yes category
+if member == 'y':
+    member = 1
+elif member == 'Y':
+    member = 2
+elif member == 'yes':
+    member = 3
+elif member == 'Yes':
+    member = 4
+# No category
+elif member == 'n':
+    member = 6
+elif member == 'N':
+    member = 7
+elif member == 'no':
+    member = 8
+elif member == 'No':
+    member = 8
 
-member = str(input('Do you have a club card?'))
-
-tax = float(input("Enter tax rate:")) / 100
+tax = float(input("Enter tax rate, e.g. 5.5 for 5.5% tax:")) / 100
 
 total2 = base2 + (base2 * tax)
 total2 = "{:,.2f}".format(total2)
 
-if member == 'y' or 'Y' or 'yes' or 'Yes':
-    y = (base2 - (base2 * 0.10))
-    total = float(y + y * tax)
+if member < 5:
+    discount = (base2 - (base2 * 0.10))
+    total = float(discount + discount * tax)
     total = "{:2.2f}".format(total)
+    discount = "{:2.2f}".format(discount)
 
-    print("Base price =", base)
-    y = "{:2.2f}".format(y)
-    print("Price after discounts =", y)
-    print("Total price =", total)
+    print("Base price =", base, "Price after discounts =", discount, "Total price =", total)
 
-elif member != 'y' or 'Y' or 'yes' or 'Yes':
-    print("Base price =", base)
-    print("Total price = ", total2)
+elif member > 5:
+    print("Base price =", base, "Total price =", total2)
