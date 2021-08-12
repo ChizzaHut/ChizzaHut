668fa0edb061a4f9d98f193d6f3821e096adf0d2
ChizzaHut <87155392+ChizzaHut@users.noreply.github.com>
2021-08-12 13:01:59 -0400

    Update cashregister.py
    if member == 'y' or member == 'Y' or member == 'yes' or member == 'Yes':
        is_a_member = 1
    
    # No category
    elif member == 'n' or member == 'N' or member == 'no' or member == 'No':
        is_a_member = 2

--- cashregister.py
+++ cashregister.py
@@ -5,42 +5,40 @@ base = "{:,.2f}".format(base)
 
 a = min(item1, item2)
 b = max(item1, item2)
-a1 = a*.5
+a1 = a * .5
 
 base2 = a1 + b
+
 # Make variables for Y and N
 member = input('Does customer have a club card? (Y/N):')
+
+is_a_member = 0
 # Yes category
-if member == 'y':
-    member = 1
-elif member == 'Y':
-    member = 2
-elif member == 'yes':
-    member = 3
-elif member == 'Yes':
-    member = 4
+if member == 'y' or member == 'Y' or member == 'yes' or member == 'Yes':
+    is_a_member = 1
+
 # No category
-elif member == 'n':
-    member = 6
-elif member == 'N':
-    member = 7
-elif member == 'no':
-    member = 8
-elif member == 'No':
-    member = 8
+elif member == 'n' or member == 'N' or member == 'no' or member == 'No':
+    is_a_member = 2
 
 tax = float(input("Enter tax rate, e.g. 5.5 for 5.5% tax:")) / 100
 
 total2 = base2 + (base2 * tax)
 total2 = "{:,.2f}".format(total2)
 
-if member < 5:
+if is_a_member == 1:
     discount = (base2 - (base2 * 0.10))
     total = float(discount + discount * tax)
     total = "{:2.2f}".format(total)
     discount = "{:2.2f}".format(discount)
+    base2 = "{:,.2f}".format(base2)
 
     print("Base price =", base, "Price after discounts =", discount, "Total price =", total)
 
-elif member > 5:
-    print("Base price =", base, "Total price =", total2)
+elif is_a_member == 2:
+    base2 = "{:,.2f}".format(base2)
+    print("Base price =", base, "Price after discounts =", base2, "Total price =", total2)
+
+else:
+    print("Invalid")
+
